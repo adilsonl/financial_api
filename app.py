61b29d0f8d41d9a4b4302d83dcb8c29de2ad6ae5
Adilson Lima de Melo Junior <adilsonlimejr17@gmail.com>
2020-07-23 11:41:20 -0300

    Validation of inputs

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,request
-from flask_restful import Resource,Api
+from flask import Flask, request
+from flask_restful import Resource, Api
 import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-API_KEY='NW84PZQEPS9SN9S7'
+API_KEY = 'NW84PZQEPS9SN9S7'
 
 
 class PointsIbovespa(Resource):
@@ -13,22 +13,31 @@ class PointsIbovespa(Resource):
         points = searchCompaniesPoints()
         return points
 
+
 class CompanyPoints(Resource):
-    def get(self,company):
+    def get(self, company):
         points = searchCompaniesPoints(company)
         return points
 
-def searchCompaniesPoints(company="BOVA11.SAO"):
-    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey={}".format(company,API_KEY)
-    callApi = requests.get(url)
-    response = callApi.json()
-    points = {
-        "status": callApi.status_code,
-        "points": response["Global Quote"]["05. price"]
-    }
-    return points
 
-api.add_resource(PointsIbovespa,"/points/ibovespa")
-api.add_resource(CompanyPoints,"/points/<company>")
+def searchCompaniesPoints(company="BOVA11.SAO"):
+    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey={}".format(company, API_KEY)
+    callApi = requests.get(url)
+    if callApi.status_code == 200:
+        response = callApi.json()
+        if 'Global Quote' in response and "05. price" in response["Global Quote"]:
+            points = {
+                "status": callApi.status_code,
+                "points": response["Global Quote"]["05. price"]
+            }
+            return points
+        else:
+            return {'error': 'Inform a valid company'}
+    else:
+        return {'error': 'Something went wrong'}
+
+
+api.add_resource(PointsIbovespa, "/ibovespa")
+api.add_resource(CompanyPoints, "/points/<company>")
 if __name__ == "__main__":
     app.run(debug=True)
