f5346a8dd76ab723107224bd41f67b660c4d95f2
Adilson Lima de Melo Junior <adilsonlimejr17@gmail.com>
2020-07-27 09:08:50 -0300

    Front-end

---
+++ front/js/main.js
@@ -0,0 +1,51 @@
+function get_ibovespa(){
+    $.ajax({
+        url:'http://127.0.0.1:5000/ibovespa',
+        type:"GET",
+        success:function(response){
+            console.log(response)
+            $('#nameCompany').html("Ibovespa")
+            $('#pointsCompany').html("Pontos: "+response.points)
+
+        }
+    })
+}
+function get_company_points(symbol,company){
+    $.ajax({
+        url:`http://127.0.0.1:5000/points/${symbol}`,
+        type:"GET",
+        success:function(response){
+            console.log(response)
+            $('#nameCompany').html(company)
+            $('#pointsCompany').html("Pontos: "+response.points)
+
+        }
+    })
+}
+
+function serachCompanies(){
+    var company = document.getElementById('searchCompany').value;
+    clearResults()
+    $.ajax({
+        url:`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${company}&apikey=NW84PZQEPS9SN9S7`,
+        type:"GET",
+        success:function(response){
+            console.log(response)
+            var bestMatches = response.bestMatches;
+            if(bestMatches.length > 0){
+                for(match of bestMatches){
+                    var functionButton = `get_company_points("${match['1. symbol']}","${match['2. name']}")`
+                    var $button = $(`<button type="button" class="btn btn-secondary" onclick='${functionButton}'>${match['2. name']}</button>`)
+                    $button.appendTo($("#results"));
+                }
+            }else{
+                var $text = $('<br><h2 id="warning">Sem resultados</h2>');
+                $text.appendTo($("#results"));
+            }
+        }
+    })
+}
+
+function clearResults(){
+    $("#results").empty()
+}
